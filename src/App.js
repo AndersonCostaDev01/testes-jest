97b12ed92e34a9ac55ab520c51f796a45429dbf3
Anderson Costa <costaandersom23@gmail.com>
2024-11-22 18:03:14 -0300

    explorando testes no react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
-import logo from './logo.svg';
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [tarefas , setTarefas] = useState([]);
+  const [tempTarefa, setTempTarefa] = useState("");
 
-  useEffect(() => {
-    const moveRandomly = () => {
-      const randomX = Math.random() * (window.innerWidth - 100); // Subtraímos a largura aproximada do logo
-      const randomY = Math.random() * (window.innerHeight - 100); // Subtraímos a altura aproximada do logo
-      setPosition({ top: randomY, left: randomX });
-    };
-
-    const interval = setInterval(moveRandomly, 1000); // Move a cada 1 segundo
-
-    return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
-  }, []);
-
+  function cadastrarTarefa() {
+    setTarefas([...tarefas, tempTarefa]);
+    setTempTarefa("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt={logo}
-          style={{
-            position: "absolute",
-            top: position.top,
-            left: position.left,
-            transition: "top 0.01s ease, left 0.1s ease",
-          }}
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text" 
+        value={tempTarefa}
+        onChange={(e) => setTempTarefa(e.target.value)}
+        data-testid="campo-tarefa"
+      />
+      <button 
+        type="button"
+        onClick={cadastrarTarefa}
+        data-testid="btn-cadastrar"
+      >Cadastrar</button>
+      <ul>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('teste do App', () => {
+  test('Deve renderizar o componente App', () => {
+    render(<App />)
+    expect(screen.getByText('Cadastrar')).toBeInTheDocument()
+  })
+  test('Deve adicionar uma tarefa', () => {
+    const {debug} = render(<App />)
+    fireEvent.change(screen.getByTestId('campo-tarefa'), {
+      target: {
+        value: 'Estudar React'
+      }
+    })
+    fireEvent.click(screen.getByTestId('btn-cadastrar'))
+    expect(screen.getByText('Estudar React')).toBeInTheDocument()
+    debug()
+  })
+})
